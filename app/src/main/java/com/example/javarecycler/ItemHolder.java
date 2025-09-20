26c98596e804f24e91136a4c5acd4cb5de9feed0
owner <191548874+JTaguiamdev@users.noreply.github.com>
2025-09-21 01:24:06 +0800

    Initial commit - JavaRecycler project

---
+++ app/src/main/java/com/example/javarecycler/ItemHolder.java
@@ -0,0 +1,17 @@
+package com.example.javarecycler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemHolder extends RecyclerView.ViewHolder {
+
+    TextView textView;
+
+    public ItemHolder(@NonNull View itemView) {
+        super(itemView);
+        TextView textView = itemView.findViewById(R.id.textView);
+    }
+}
---
+++ app/src/main/java/com/example/javarecycler/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.javarecycler;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<String> list = new ArrayList<>();
+    ItemAdapter adapter;
+    EditText txtName;
+    Button btnAdd;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ///
+            recyclerView = findViewById(R.id.recyclerView);
+            txtName = findViewById(R.id.editText1);
+            btnAdd = findViewById(R.id.button);
+            adapter = new ItemAdapter(list, this);
+            recyclerView.setAdapter(adapter);
+            //
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String name = txtName.getText().toString();
+                    list.add(name);
+                    adapter.notifyDataSetChanged();
+
+                }
+            });
+        ///
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
